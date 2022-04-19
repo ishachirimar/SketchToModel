57afaabfb04ee188564bc75396cb7a40d9124a4a
Jack Otto <jmo97@cornell.edu>
2022-04-18 20:02:26 -0400

    First Data Setgit add --all woop woop!

--- Test_JO.py
+++ Test_JO.py
@@ -1,7 +1,7 @@
 import ProbMat
 
 path = "C:\\Users\\jackm\Documents\\Cornell 21-22\\2_ITHACA\\SketchToModel\\TrainingData\\Voxels\\"
-name = "SpotOut_8000_2.txt"
+name = "Test_Vox_0000.txt"
 
 "read file returns a [value matrix, grid] pair"
 myprobmat = ProbMat.readProbMat(path+name)
@@ -12,4 +12,4 @@ ProbMat.dotplot(myprobmat, 0)
 
 path = "C:\\Users\\jackm\Documents\\Cornell 21-22\\2_ITHACA\\SketchToModel\\"
 name = "myobjtest.obj"
-ProbMat.meshprobmat(myprobmat, .25, path, name)
+ProbMat.meshprobmat(myprobmat, .25, path, name, True)
